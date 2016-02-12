ae2fb2e2813b733d5b28a18614403516753c122a
geoffles <geoffles@gmail.com>
2016-02-12 09:32:36 +0200

    Revised the view to use a grid. Did left join on all items

--- Smaller/TaskListWindow.xaml.cs
+++ Smaller/TaskListWindow.xaml.cs
@@ -16,11 +16,32 @@ namespace Smaller
     /// <summary>
     /// Interaction logic for TaskList.xaml
     /// </summary>
+    
     public partial class TaskListWindow : Window
     {
         private TaskListWindowViewModel Model = null;
         public TaskListWindow()
         {
+            bool designTime = System.ComponentModel.DesignerProperties.GetIsInDesignMode(this);
+            if (designTime)
+            {
+                DataContext = new TaskListWindowViewModel()
+                {
+                    Things = new TaskListWindowViewModel.Job[]
+                    {
+                        new TaskListWindowViewModel.Job
+                        {
+                            Id = "The ID",
+                            Result = "Success",
+                            RunDate = new DateTime(2016, 01, 04),
+                            ScheduledDate = new DateTime(2016, 01, 03),
+                            Type = "Mail"
+                        }
+                    }
+                };
+            }
+
+
             InitializeComponent();
         }
 
--- Smaller/TaskListWindowViewModel.cs
+++ Smaller/TaskListWindowViewModel.cs
@@ -8,21 +8,34 @@ namespace Smaller
 {
     public class TaskListWindowViewModel
     {
-        private TaskListWindowViewModel()
+        private static RunHistory Null(RunHistory history)
+        {
+            return history ?? new RunHistory {
+                Identifier = string.Empty,
+                Result = string.Empty,
+                RunDate = DateTime.MaxValue
+            };
+        }
+
+        public TaskListWindowViewModel()
         {
         }
 
         public static TaskListWindowViewModel From(SmallerTaskList tasks, RunHistoryList history)
         {
             var jobs = tasks.Tasks
-                .Join(history, o => o.Identifier, i => i.Identifier, (o, i) => new {Task = o, History = i})
+                .GroupJoin(history, 
+                    o => o.Identifier, i => i.Identifier, 
+                    (o, i) => new {Task = o, History = i})
+                .SelectMany(io => io.History.DefaultIfEmpty(),
+                    (o,i) => new {Task = o.Task, History=i})
                 .Select(p => new Job
                 {
                     Id = p.Task.Identifier,
                     Type = p.Task.GetType().Name,
-                    RunDate = p.History.RunDate,
+                    RunDate = p.History == null ? null : (DateTime?)p.History.RunDate,
                     ScheduledDate = p.Task.ScheduledDate,
-                    Result = p.History.Result
+                    Result = Null(p.History).Result
                 });
 
             return new TaskListWindowViewModel
@@ -36,7 +49,7 @@ namespace Smaller
             public string Id { get; set; }
             public string Type { get; set; }
             public DateTime ScheduledDate { get; set; }
-            public DateTime RunDate { get; set; }
+            public DateTime? RunDate { get; set; }
             public string Result { get; set; }
         }
 
